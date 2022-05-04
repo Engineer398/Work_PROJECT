b87435320d4b60cad90d2ffd8674c6d143f82db4
zzz <099025zzz@gmail.com>
2022-05-04 22:42:45 +0300

    ReNew

---
+++ main.py
@@ -0,0 +1,17 @@
+class Test:
+    __slots__ = []
+    def __init__(self):
+        self.a = 1
+
+    def doSome(self):
+        print("Do Some")
+
+a = Test()
+b = Test()
+
+print(b.__dict__)
+del b.a
+print(b.__dict__)
+
+
+
